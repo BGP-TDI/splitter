3f53f797a4bf6ace9f3f4e3bb6d224b87921274d
Pawel Foremski <pjf@iitis.pl>
2022-01-11 10:53:38 +0100

    add -L/-R options + require Go 1.18
     - use the built-in netip instead of netaddr (requires Go 1.18)
     - -L/-R can be used to split the generated tree of prefixes evenly in half, in a worst possible way
       from BGP aggregation POV

--- go.mod
+++ go.mod
@@ -1,9 +1,3 @@
-module example.com/bgp/splitter
+module github.com/BGP-TDI/splitter
 
-go 1.17
-
-require (
-	go4.org/intern v0.0.0-20210108033219-3eb7198706b2 // indirect
-	go4.org/unsafe/assume-no-moving-gc v0.0.0-20201222180813-1025295fd063 // indirect
-	inet.af/netaddr v0.0.0-20210603230628-bf05d8b52dda
-)
+go 1.18
--- main.go
+++ main.go
@@ -4,16 +4,28 @@ import (
 	"flag"
 	"fmt"
 	"log"
-
-	"inet.af/netaddr"
+	"net/netip"
 )
 
-func split(ipbytes [16]byte, plen uint8) {
+var (
+	opt_L bool
+	opt_R bool
+)
+
+func split(ipbytes [16]byte, plen int, left bool) {
 	// print
-	ipaddr := netaddr.IPFrom16(ipbytes)
+	ipaddr := netip.AddrFrom16(ipbytes)
 	ipprefix, err := ipaddr.Prefix(plen)
 	if err != nil { log.Fatal(err) }
-	fmt.Println(ipprefix)
+
+	switch {
+	case opt_L:
+		if left  { fmt.Println(ipprefix) }
+	case opt_R:
+		if !left { fmt.Println(ipprefix) }
+	default:
+		fmt.Println(ipprefix)
+	}
 
 	if plen < 48 {
 		i := plen / 8
@@ -21,29 +33,31 @@ func split(ipbytes [16]byte, plen uint8) {
 
 		// set to 0
 		ipbytes[i] &= ^(1 << j)
-		split(ipbytes, plen+1)
+		split(ipbytes, plen+1, true)
 
 		// set to 1
 		ipbytes[i] |= (1 << j)
-		split(ipbytes, plen+1)
+		split(ipbytes, plen+1, false)
 	}
 }
 
 func main() {
+	flag.BoolVar(&opt_L, "L", false, "print only the prefixes after a split on 0 (left-hand side)")
+	flag.BoolVar(&opt_R, "R", false, "print only the prefixes after a split on 1 (right-hand side)")
 	flag.Parse()
 
 	arg1 := flag.Arg(0)
 	if len(arg1) == 0 { log.Fatal("pass IPv6 prefix as the 1st arg") }
 
-	ipprefix, err := netaddr.ParseIPPrefix(arg1)
+	ipprefix, err := netip.ParsePrefix(arg1)
 	if err != nil { log.Fatalf("parsing IP prefix in the 1st arg: %s", err) }
 
 	ipprefix = ipprefix.Masked()
-	ipaddr := ipprefix.IP().Unmap()
+	ipaddr := ipprefix.Addr().Unmap()
 	if !ipaddr.Is6() { log.Fatalf("%s: not an IPv6 prefix", ipprefix) }
 
 	ipbytes := ipaddr.As16()
 	plen := ipprefix.Bits()
 	// fmt.Printf("parsed: %s\nraw: %x / %d\n", ipprefix, ipbytes, plen)
-	split(ipbytes, plen)
+	split(ipbytes, plen, true)
 }
