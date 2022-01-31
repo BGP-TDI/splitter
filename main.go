eeb9e7e75d0ebe01de76725670834479a5f65aeb
Oliver Gasser <oliver.gasser@mpi-inf.mpg.de>
2022-01-31 16:00:03 +0100

    add options to specify max prefix length and prefix count

--- main.go
+++ main.go
@@ -8,12 +8,19 @@ import (
 )
 
 var (
-	opt_L bool
-	opt_R bool
+	opt_L      bool
+	opt_R      bool
+	opt_plen   int
+	opt_count  int
+	curr_count int = 0
 )
 
 func split(ipbytes [16]byte, plen int) {
-	if plen >= 48 {
+	if plen >= opt_plen {
+		return
+	}
+
+	if (opt_count != -1) && (curr_count >= opt_count) {
 		return
 	}
 
@@ -23,21 +30,27 @@ func split(ipbytes [16]byte, plen int) {
 	// set to 0
 	ipbL := ipbytes
 	ipbL[i] &= ^(1 << j)
-	prefixL, err := netip.AddrFrom16(ipbL).Prefix(plen+1)
+	prefixL, err := netip.AddrFrom16(ipbL).Prefix(plen + 1)
 	if err != nil {
 		log.Fatal(err)
 	} else if opt_L || !opt_R {
 		fmt.Println(prefixL)
+		curr_count += 1
+	}
+
+	if (opt_count != -1) && (curr_count >= opt_count) {
+		return
 	}
 
 	// set to 1
 	ipbR := ipbytes
 	ipbR[i] |= (1 << j)
-	prefixR, err := netip.AddrFrom16(ipbR).Prefix(plen+1)
+	prefixR, err := netip.AddrFrom16(ipbR).Prefix(plen + 1)
 	if err != nil {
 		log.Fatal(err)
 	} else if !opt_L || opt_R {
 		fmt.Println(prefixR)
+		curr_count += 1
 	}
 
 	// recurse
@@ -48,17 +61,25 @@ func split(ipbytes [16]byte, plen int) {
 func main() {
 	flag.BoolVar(&opt_L, "L", false, "print only the prefixes after a split on 0 (left-hand side)")
 	flag.BoolVar(&opt_R, "R", false, "print only the prefixes after a split on 1 (right-hand side)")
+	flag.IntVar(&opt_plen, "l", 48, "most specific prefix length to generate")
+	flag.IntVar(&opt_count, "c", -1, "maximum number of prefixes to generate")
 	flag.Parse()
 
 	arg1 := flag.Arg(0)
-	if len(arg1) == 0 { log.Fatal("pass IPv6 prefix as the 1st arg") }
+	if len(arg1) == 0 {
+		log.Fatal("pass IPv6 prefix as the 1st arg")
+	}
 
 	ipprefix, err := netip.ParsePrefix(arg1)
-	if err != nil { log.Fatalf("parsing IP prefix in the 1st arg: %s", err) }
+	if err != nil {
+		log.Fatalf("parsing IP prefix in the 1st arg: %s", err)
+	}
 
 	ipprefix = ipprefix.Masked()
 	ipaddr := ipprefix.Addr().Unmap()
-	if !ipaddr.Is6() { log.Fatalf("%s: not an IPv6 prefix", ipprefix) }
+	if !ipaddr.Is6() {
+		log.Fatalf("%s: not an IPv6 prefix", ipprefix)
+	}
 
 	ipbytes := ipaddr.As16()
 	plen := ipprefix.Bits()
